9258fd2394e83d8ed47156786588240af315c680
Abdel Yasser <zahabys@gmail.com>
2024-02-14 19:51:45 -0700

    Initial commit

---
+++ OpenGL/src/Application.cpp
@@ -0,0 +1,66 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+    
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "ERROR!" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+       -0.5f, -0.5f,
+        0.0f, 0.0f,
+        0.5f, -0.5f
+    };
+    
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    //generates one buffer at a time provide an address to an integer (unsigned)
+    //so it can write it's ID to that vraible 
+    //everything in openGL that is generated (contextially) gets assigned a unique identifier
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    //selecting that buffer by its ID
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    //given a target buffer and its intended size/ size of data going into buffer
+    //you also have to provide a pointer to the data that will be coppied
+    //and usage, go to docs.gl and you can find all function info
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate(); 
+    return 0;
+}
