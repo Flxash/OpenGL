7507e73fc334b08dfbf82ed7fc4a61f2aad7e11e
Abdel Yasser <zahabys@gmail.com>
2024-02-15 12:29:23 -0700

    Prelim Shaders implemented for red triangle

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -3,6 +3,48 @@
 
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        //alloca is a function that c gives that klets you allocate on the stack dynamically
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -49,6 +91,43 @@ int main(void)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+    //now what is a shader: program that runs on the gpu -> parralell algorithms
+
+    //the vertex shader basically takes in a set of vertecies and formats them in a way and prepares them as vertecies
+    //this step is not redundant as it sets the vertecies properly
+    //the fragment shader is basically what fills in individual pixels in a specieifeied vertex bound space
+    //the space is bound by the vertecies provided by our vertex shader
+    //the fragment shader rasterizes. which means the pixels get "shaded in" like a coloring book
+    //the fragment shader gets called soooooo many times the vertex shader only gets callled a cpuple time in comparison
+    //fragment shaders are a lot more expensive so try to do as many operations possible in the vertex shader
+    //obv thats not always possible because some operations are needed to be done on a pixel basis (lighting)
+    //our current pipeline: vertex shader -> fragment shader
+
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    //GLSL files as strings 
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -63,6 +142,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate(); 
     return 0;
