3e8ae681afac9de153d65602c5fd2ed9e3ba5e51
Abdel Yasser <zahabys@gmail.com>
2024-02-18 22:48:51 -0700

    Added renderer

--- OpenGL/src/Renderer.h
+++ OpenGL/src/Renderer.h
@@ -2,6 +2,12 @@
 
 #include<GL/glew.h>
 
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
+
+#include <iostream>
+
 //macros:
 //this is an intresic to the compiler so its different for each compiler
 // this is using msvc compiler (identified by the "__")
@@ -13,3 +19,10 @@
 
 void GLClearError();
 bool GLLogCall(const char* function, const char* file, int line);
+
+class Renderer
+{
+public:
+    void Clear() const;
+    void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+};
